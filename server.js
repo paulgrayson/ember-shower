0e7681fd4479c6aa086283bef1701aa2e0e6b08e
Paul Grayson <paul@graysontechnology.co.uk>
2013-08-16 11:34:23 +0100

    Move handlebards dirs into express public

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http'),
-    path = require('path'),
-    fs = require('fs');
-
-http.createServer(function(request, response) {
-  var lookup = path.basename(decodeURI(request.url)) || 'index.html';
-  var filename = 'public/' + lookup
-});
-
